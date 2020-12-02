b22174ae288d257af3d4b98153f65fde6f15d12e
hrtain <hrtain1@gmail.com>
2020-12-02 21:17:55 +0800

    update

---
+++ data-structures-javascript/Link_List/linklist.js
@@ -0,0 +1,29 @@
+// class List {
+//     constructor(size) {
+//         this.data = [];
+//         this.Size = size;
+//         this.length = 0;
+//     }
+
+//     clear() {
+//         this.Size = 0;
+//         this.data = [];
+//     }
+
+//     append(element) {
+//         this.data[this.Size++] = element;
+//     }
+
+//     remove(element) {
+//         this.data = this.data.filter((item) => item !== element);
+//         this.size = this.data.length;
+//     }
+// }
+
+// let list = new List();
+// list.append(1);
+// console.log(list.data, list);
+
+for(let i = 0; i < 6; i++) {
+    console.log(Math.random()* 11 + 44)
+}
